601df66e38212d38cb671dd6ede79ebc653be436
***** <gsood07@gmail.com>
2021-07-06 13:39:58 -0700

    add t20i

--- README.md
+++ README.md
@@ -4,19 +4,31 @@ The ICC only lists the [current ranking of cricket teams](https://www.icc-cricke
 
 ### Data, Scripts, and Outputs
 
-To build the [dataset of matches](data/), we used the [python wrapper to espncricinfo](https://github.com/outside-edge/python-espncricinfo).
+To build the [dataset of matches](data/cricket_matches.csv), we used the [python wrapper to espncricinfo](https://github.com/outside-edge/python-espncricinfo).
 
-* [ELO](scripts/elo.R)
+* [ELO Script](scripts/elo.R)
 
-**Test Ratings as of 6/18/2021**
+**Men's Test Team Ratings as of 6/18/2021**
 
-![Test ratings as of 6/18/2021](figs/test_ratings_2021-06-18.png)
+![Men's Test ratings as of 6/18/2021](figs/test_ratings_2021-06-18.png)
 
-**Smoothed Test Ratings Over Time (1881--2021)**
+**Smoothed Men's Test Team Ratings (1881--2021)**
 
-![Test ratings over time](figs/test_ratings_1881_2021.png.png)
+![Men's Test ratings over time](figs/test_ratings_1881_2021.png)
 
-Download the complete monthly time series dataset [here](data/)
+Download the complete monthly ratings [here](data/test_ratings_1881_2021.csv)
+
+**Smoothed Men's ODI Team Ratings (1972--2021)**
+
+![Men's ODI ratings over time](figs/odi_ratings_1972_2021.png)
+
+Download the complete monthly ratings [here](data/odi_ratings_1972_2021.csv)
+
+**Smoothed Men's T20I Team Ratings (2006---2021)**
+
+![Men's T20I ratings over time](figs/t20i_ratings_2006_2021.png)
+
+Download the complete monthly ratings [here](data/t20i_ratings_2006_2021.csv)
 
 ### Authors
 
--- scripts/elo.R
+++ scripts/elo.R
@@ -148,7 +148,7 @@ for(i in month_ts[1:length(month_ts)-1]) {
 write_csv(odi_res_df, file = "../data/odi_ratings_1972_2021.csv")
 
 # Only keeping teams with more than 100 matches
-odi_teams <- table(c(cric_odis$team1, cric_odis$team2))
+odi_teams <- table(c(cric_odis$team1, cric_odis$team2)) # Some issues that we want to fix later
 small_odi_df <- odi_res_df[(odi_res_df$teams %in%  names(odi_teams[odi_teams > 200])), ]
 
 p2 = ggplot(small_odi_df, aes(x = date, y = m_ratings, color = teams)) +
@@ -170,3 +170,63 @@ gt2 <- ggplotGrob(p2)
 gt2$layout$clip[gt2$layout$name == "panel"] <- "off"
 grid.draw(gt2)
 ggsave("../figs/odi_ratings_1972_2021.png", plot = gt2)
+
+# T20Is
+# ------------------
+cric_t20is <- subset(cric, cric$type_of_match == "T20I")
+cric_t20is <- cric_t20is[order(cric_t20is$Date), ] # for EloRating
+
+with(cric_t20is, seqcheck(winner = winner,
+	                      loser  = loser,
+	                      draw   = drawn,
+	                      Date   = Date))
+
+t20i_res <- with(cric_t20is, elo.seq(winner = winner,
+	                      loser  = loser,
+	                      draw   = drawn,
+	                      Date   = Date))
+
+summary(t20i_res)
+
+# Let's get average ratings per month
+# Iterate over all the months
+# Output is Long DF
+t20i_res_df <- data.frame(teams = NULL, date = NULL, m_ratings = NULL)
+min_year <- format(as.POSIXct(t20i_res$misc["minDate"], format = "%Y-%m-%d"), format = "%Y")
+max_year <- format(as.POSIXct(t20i_res$misc["maxDate"], format = "%Y-%m-%d"), format = "%Y")
+month_ts <- seq(as.Date(t20i_res$misc["minDate"]), as.Date(t20i_res$misc["maxDate"]), by = "month")
+
+for(i in month_ts[1:length(month_ts)-1]) {
+	m_ratings <- extract_elo(t20i_res, 
+		        NA.interpolate = T, 
+		        extractdate	= as.Date(i),
+		        daterange = 30) # We are smoothing ratings by year
+	m_ratings_df <- data.frame(m_ratings, teams = names(m_ratings), date = as.Date(i), row.names = NULL)
+	t20i_res_df <- rbind(t20i_res_df, m_ratings_df)
+}
+
+write_csv(t20i_res_df, file = "../data/t20i_ratings_2006_2021.csv")
+
+# Only keeping teams with more than 50 matches
+t20i_teams <- table(c(cric_t20is$team1, cric_t20is$team2)) # Some issues that we want to fix later
+small_t20i_df <- t20i_res_df[(t20i_res_df$teams %in%  names(t20i_teams[t20i_teams > 100])), ]
+
+p2 = ggplot(small_t20i_df, aes(x = date, y = m_ratings, color = teams)) +
+	geom_smooth(method = "loess", se = F,  span = .1, size = .5)+ 
+	scale_color_brewer(palette = "Paired") +
+	ylab("ELO Ratings") + 
+	xlab("") + 
+	scale_x_continuous(breaks = seq(min(month_ts), max(month_ts) + 10, length.out = 15),
+					   labels = format(seq(min(month_ts), max(month_ts), length.out = 15), "%Y")) + 
+	scale_y_continuous(breaks = round(seq(min(small_t20i_df$m_ratings, na.rm = T), max(small_t20i_df$m_ratings, na.rm = T), by = 100), digits = -1),
+                       labels = round(seq(min(small_t20i_df$m_ratings, na.rm = T), max(small_t20i_df$m_ratings, na.rm = T), by = 100), digits = -1)) + 
+	theme_minimal() + 
+	theme(legend.position = "none",
+          plot.margin = unit(c(1,4,1,1), "lines")) +
+	geom_dl(aes(label = teams), method = list("last.points", cex = 0.6, vjust = 0)) +
+	ggtitle("Smoothed ELO Ratings Over Time")
+
+gt2 <- ggplotGrob(p2)
+gt2$layout$clip[gt2$layout$name == "panel"] <- "off"
+grid.draw(gt2)
+ggsave("../figs/t20i_ratings_2006_2021.png", plot = gt2)
